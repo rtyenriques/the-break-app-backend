eef52d6afb43b6fea3da02c618f0869fa8755b67
= <rtyenriques@gmail.com>
2022-03-28 23:10:20 -0700

    created method routes for categories controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::ArticlesController < ApplicationController
+
+    before_action :set_category
+    
+
+    def index
+        @articles = Article.all
+        render json: @articles
+
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            render json: @category
+        else
+            render json: {error: 'Error creating category'}
+
+    end
+
+    def show
+        @article = Article.find(params[:id])
+        render json: @article
+
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :category_id, :body, :subtitle, :author, :publish_date, :image)
+        
+    end
+
+    def set_category
+        @category = Category.find(params[:id])
+    end
 end
--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::CategoriesController < ApplicationController
+
+    def index
+        @categories = Category.all
+        render json: @categories
+
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            render json: @category
+        else
+            render json: {error: 'Error creating category'}
+
+    end
+
+    def show
+        @category = Category.find(params[:id])
+        render json: @category
+
+    end
+
+    def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+        
+    end
+
+    def set_category
+        @category = Category.find(params[:id])
+    end
 end
